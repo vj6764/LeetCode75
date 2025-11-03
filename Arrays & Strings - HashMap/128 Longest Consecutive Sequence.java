94b2ba9b0c0881c1a964aec96e51b8ef2cad1eb7
vj_padala <vj6764@gmail.com>
2025-11-03 20:11:16 +0530

    Implement longest consecutive sequence algorithm
    Leetcode link: https://leetcode.com/problems/longest-consecutive-sequence/description/

---
+++ Arrays & Strings - HashMap/128 Longest Consecutive Sequence.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest consecutive sequence length: " + longestConsecutive(nums));
+    }
+}
