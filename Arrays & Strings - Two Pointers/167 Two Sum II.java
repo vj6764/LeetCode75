8f028d17df0c5c8d727a88003cf15988d2feb1f8
vj_padala <vj6764@gmail.com>
2025-11-06 21:58:36 +0530

    Implement twoSum method using two pointers
    leet code link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

---
+++ Arrays & Strings - Two Pointers/167 Two Sum II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        
+        while (left < right) {
+            int currentSum = numbers[left] + numbers[right];
+            
+            if (currentSum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return new int[] {};
+    }
+}
