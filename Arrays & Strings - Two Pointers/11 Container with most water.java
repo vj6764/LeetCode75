40c4408e715009051f9dd79c81890d0a21cc9b98
vj_padala <vj6764@gmail.com>
2025-11-10 17:26:05 +0530

    Implement maxArea function for Container with Most Water
    leet code link: https://leetcode.com/problems/container-with-most-water/description/

---
+++ Arrays & Strings - Two Pointers/11 Container with most water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int curArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, curArea);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxArea;
+    }
+}
